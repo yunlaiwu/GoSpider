5814589d123d0f530acd35f445766b6d962b94d8
liujia <liujiafrom360@163.com>
2017-11-06 00:42:44 +0800

    保存一下

--- spider/html_parser_test.go
+++ spider/html_parser_test.go
@@ -3,8 +3,10 @@ package main_test
 import (
     "testing"
     spider "GoSpider/spider"
+    "fmt"
 )
 
+/*
 func Test_ParseBookComment(t *testing.T) {
     htm, err := spider.HttpGet("https://book.douban.com/subject/1083428/comments/new")
     if err != nil {
@@ -13,6 +15,7 @@ func Test_ParseBookComment(t *testing.T) {
 
     spider.ParseBookComment(string(htm))
 }
+*/
 
 func Test_ParseBookReview(t *testing.T) {
     htm, err := spider.HttpGet("https://book.douban.com/subject/1083428/reviews?sort=time")
@@ -20,5 +23,21 @@ func Test_ParseBookReview(t *testing.T) {
         t.FailNow()
     }
 
-    spider.ParseBookReview(string(htm))
+    reviewIds, err := spider.ParseBookReviewListPage(string(htm))
+    if err != nil {
+        t.FailNow()
+    }
+
+    for _,reviewId := range reviewIds {
+        fmt.Println(reviewId)
+    }
+
+    reviewId := reviewIds[0]
+    reviewUrl := fmt.Sprintf("https://book.douban.com/review/%v/#comments", reviewId)
+    fmt.Println("review full url", reviewUrl)
+    htm, err = spider.HttpGet(reviewUrl)
+    if err != nil {
+        t.FailNow()
+    }
+    spider.ParseBookReviewPage(string(htm))
 }
--- spider/utils.go
+++ spider/utils.go
@@ -9,6 +9,7 @@ import (
     "errors"
     "time"
     "fmt"
+    "path/filepath"
 )
 
 func ReadFileLines(fi *os.File) (ret *list.List, err error) {
@@ -47,4 +48,22 @@ func Time2String(ts int64) string  {
     var left = ts%1000
     var s string = time.Unix(secs, 0).Format("2006-01-02 15:04:05")
     return s+fmt.Sprintf(":%v", left)
+}
+
+//得到当前工作目录，其实就是当前进程的work directory
+func GetCurrentDirectory() string {
+    dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+    if err != nil {
+        fmt.Println(err)
+    }
+    return strings.Replace(dir, "\\", "/", -1)
+}
+
+//得到全路径，如果relPath已经是Absolute的就直接用，否则跟当前工作目录拼成Abs的
+func GetFullPath(relPath string) (fullPath string) {
+    if filepath.IsAbs(relPath) == false {
+        return filepath.Join(GetCurrentDirectory(), relPath)
+    } else {
+        return relPath
+    }
 }
