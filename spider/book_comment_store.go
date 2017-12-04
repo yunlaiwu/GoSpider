e3a74b00022664b9f8e0ea5a7246faf2e24efb24
liujia <liujia@LiuJia-MacBook-Pro.local>
2017-12-04 17:30:45 +0800

    liujia

--- spider/book_comment_store.go
+++ spider/book_comment_store.go
@@ -9,123 +9,123 @@ package main
  */
 
 import (
-    "container/list"
-    "os"
-    "sync"
-    "strings"
-    "path/filepath"
+	"container/list"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
 )
 
 type BookCommentStore struct {
-    booksFile string
-    saveDir  string
+	booksFile string
+	saveDir   string
 
-    bookList *list.List
-    bookListLock sync.Mutex
-    doneMap  sync.Map
-    totalCount int
-    doneCount int
+	bookList     *list.List
+	bookListLock sync.Mutex
+	doneMap      sync.Map
+	totalCount   int
+	doneCount    int
 }
 
 func NewBookCommentStore() *BookCommentStore {
-    return &BookCommentStore{
-        bookList:list.New(),
-        totalCount:0,
-        doneCount: 0,
-    }
+	return &BookCommentStore{
+		bookList:   list.New(),
+		totalCount: 0,
+		doneCount:  0,
+	}
 }
 
 func (self *BookCommentStore) Start(booksFile, saveDir string) (err error) {
-    logInfo("BookCommentStore:Start, start")
-    self.booksFile = booksFile
-    self.saveDir = GetFullPath(filepath.Join(saveDir, "./book-comment/"))
-    err = CreateDirIfNotExist(self.saveDir)
-    if err != nil {
-        logErrorf("BookComment:saveToFile, failed to create folder %v", self.saveDir)
-        return err
-    }
+	logInfo("BookCommentStore:Start, start")
+	self.booksFile = booksFile
+	self.saveDir = GetFullPath(filepath.Join(saveDir, "./book-comment/"))
+	err = CreateDirIfNotExist(self.saveDir)
+	if err != nil {
+		logErrorf("BookComment:saveToFile, failed to create folder %v", self.saveDir)
+		return err
+	}
 
-    bookFile, err := os.Open(self.booksFile)
-    if err != nil {
-        logErrorf("BookCommentStore:Start, failed to open booksFile %v", err)
-        return err
-    }
+	bookFile, err := os.Open(self.booksFile)
+	if err != nil {
+		logErrorf("BookCommentStore:Start, failed to open booksFile %v", err)
+		return err
+	}
 
-    defer bookFile.Close()
+	defer bookFile.Close()
 
-    lines, err := ReadFileLines(bookFile)
-    if err != nil {
-        logErrorf("BookCommentStore:Start, failed to read booksFile %v", err)
-        return err
-    }
+	lines, err := ReadFileLines(bookFile)
+	if err != nil {
+		logErrorf("BookCommentStore:Start, failed to read booksFile %v", err)
+		return err
+	}
 
-    bidDone := loadDoneTask(GetFullPath(filepath.Join(self.saveDir, "./book-comment/")))
+	bidDone := loadDoneTask(self.saveDir)
 
-    for elem := lines.Front(); elem != nil; elem = elem.Next() {
-        //每行是用\t分割的 bookID和bookTitle
-        parts := strings.Split(elem.Value.(string) , "\t")
-        if len(parts) != 2 {
-            //report error here
-            continue
-        }
+	for elem := lines.Front(); elem != nil; elem = elem.Next() {
+		//每行是用\t分割的 bookID和bookTitle
+		parts := strings.Split(elem.Value.(string), "\t")
+		if len(parts) != 2 {
+			//report error here
+			continue
+		}
 
-        if _, exist := bidDone[parts[0]]; !exist {
-            self.bookList.PushBack(NewBookComment(parts[0], parts[1], self.saveDir))
-        }else {
-            logInfof("book comment for %v|%v is already downloaded", parts[0], parts[1])
-        }
-    }
+		if _, exist := bidDone[parts[0]]; !exist {
+			self.bookList.PushBack(NewBookComment(parts[0], parts[1], self.saveDir))
+		} else {
+			logInfof("book comment for %v|%v is already downloaded", parts[0], parts[1])
+		}
+	}
 
-    self.totalCount = self.bookList.Len()
-    if self.totalCount == 0 {
-        //啥都没有
-        logInfof("NO TASK! total download %v resources", self.doneCount)
-        doneChan <- nil
-    }
+	self.totalCount = self.bookList.Len()
+	if self.totalCount == 0 {
+		//啥都没有
+		logInfof("NO TASK! total download %v resources", self.doneCount)
+		doneChan <- nil
+	}
 
-    comments := self.getCommentTask(3)
-    for _, comment := range comments {
-        comment.Start()
-    }
+	comments := self.getCommentTask(3)
+	for _, comment := range comments {
+		comment.Start()
+	}
 
-    return nil
+	return nil
 }
 
 func (self *BookCommentStore) OnFinished(id string) {
-    self.doneCount +=1
-    self.doneMap.Store(id, TimeMillSecond())
+	self.doneCount += 1
+	self.doneMap.Store(id, TimeMillSecond())
 
-    logInfof("One Task is Done! downloaded %v resources now", self.doneCount)
+	logInfof("One Task is Done! downloaded %v resources now", self.doneCount)
 
-    comments := self.getCommentTask(1)
-    for _, comment := range comments {
-        comment.Start()
-    }
+	comments := self.getCommentTask(1)
+	for _, comment := range comments {
+		comment.Start()
+	}
 
-    if self.totalCount == self.doneCount {
-        //都完成了
-        logInfof("All Task is Done! total download %v resources", self.doneCount)
-        doneChan <- nil
-    }
+	if self.totalCount == self.doneCount {
+		//都完成了
+		logInfof("All Task is Done! total download %v resources", self.doneCount)
+		doneChan <- nil
+	}
 }
 
 func (self *BookCommentStore) getCommentTask(n int) (comments []*BookComment) {
-    comments = make([]*BookComment, 0)
-    if n < 1 {
-        return comments
-    }
+	comments = make([]*BookComment, 0)
+	if n < 1 {
+		return comments
+	}
 
-    for{
-        elem := self.bookList.Front()
-        if elem == nil {
-            break
-        }
-        comments = append(comments, elem.Value.(*BookComment))
-        self.bookList.Remove(elem)
-        if len(comments) == n {
-            return comments
-        }
-    }
+	for {
+		elem := self.bookList.Front()
+		if elem == nil {
+			break
+		}
+		comments = append(comments, elem.Value.(*BookComment))
+		self.bookList.Remove(elem)
+		if len(comments) == n {
+			return comments
+		}
+	}
 
-    return comments
+	return comments
 }
--- spider/book_review_store.go
+++ spider/book_review_store.go
@@ -5,137 +5,136 @@ package main
  */
 
 import (
-    "container/list"
-    "os"
-    "sync"
-    "strings"
-    "path/filepath"
+	"container/list"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
 )
 
 type BookReviewStore struct {
-    booksFile string
-    saveDir  string
+	booksFile string
+	saveDir   string
 
-    bookList *list.List
-    bookListLock sync.Mutex
-    doneMap  sync.Map
-    totalCount int
-    doneCount int
+	bookList     *list.List
+	bookListLock sync.Mutex
+	doneMap      sync.Map
+	totalCount   int
+	doneCount    int
 }
 
 func NewBookReviewStore() *BookReviewStore {
-    return &BookReviewStore{
-        bookList:list.New(),
-        totalCount:0,
-        doneCount: 0,
-    }
+	return &BookReviewStore{
+		bookList:   list.New(),
+		totalCount: 0,
+		doneCount:  0,
+	}
 }
 
 func (self *BookReviewStore) Start(booksFile, saveDir string) (err error) {
-    logInfo("BookReviewStore:Start, start")
-    self.booksFile = booksFile
-    self.saveDir = saveDir
+	logInfo("BookReviewStore:Start, start")
+	self.booksFile = booksFile
+	self.saveDir = GetFullPath(filepath.Join(saveDir, "./book-review/"))
 
-    bookFile, err := os.Open(self.booksFile)
-    if err != nil {
-        logErrorf("BookReviewStore:Start, failed to open booksFile %v", err)
-        return err
-    }
+	bookFile, err := os.Open(self.booksFile)
+	if err != nil {
+		logErrorf("BookReviewStore:Start, failed to open booksFile %v", err)
+		return err
+	}
 
-    defer bookFile.Close()
+	defer bookFile.Close()
 
-    lines, err := ReadFileLines(bookFile)
-    if err != nil {
-        logErrorf("BookReviewStore:Start, failed to read booksFile %v", err)
-        return err
-    }else {
-        logInfof("we got %v books listed in config file", lines.Len())
-    }
-    logInfof("we got %v books in task file", lines.Len())
+	lines, err := ReadFileLines(bookFile)
+	if err != nil {
+		logErrorf("BookReviewStore:Start, failed to read booksFile %v", err)
+		return err
+	} else {
+		logInfof("we got %v books listed in config file", lines.Len())
+	}
+	logInfof("we got %v books in task file", lines.Len())
 
-    bidDone := loadDoneTask(GetFullPath(filepath.Join(self.saveDir, "./book-review/")))
-    logInfof("we got %v books already downloaded", len(bidDone))
+	bidDone := loadDoneTask(self.saveDir)
+	logInfof("we got %v books already downloaded", len(bidDone))
 
-    for elem := lines.Front(); elem != nil; elem = elem.Next() {
-        //每行是用\t分割的 bookID和bookTitle
-        parts := strings.Split(elem.Value.(string) , "\t")
-        if len(parts) != 2 {
-            //report error here
-            logErrorf("invalid line in task file, %v", elem.Value.(string))
-            continue
-        }
+	for elem := lines.Front(); elem != nil; elem = elem.Next() {
+		//每行是用\t分割的 bookID和bookTitle
+		parts := strings.Split(elem.Value.(string), "\t")
+		if len(parts) != 2 {
+			//report error here
+			logErrorf("invalid line in task file, %v", elem.Value.(string))
+			continue
+		}
 
-        if _, exist := bidDone[parts[0]]; !exist {
-            self.bookList.PushBack(NewBookReview(parts[0], parts[1], self.saveDir))
-        }else {
-            logInfof("book review for %v|%v is already downloaded", parts[0], parts[1])
-        }
-    }
+		if _, exist := bidDone[parts[0]]; !exist {
+			self.bookList.PushBack(NewBookReview(parts[0], parts[1], self.saveDir))
+		} else {
+			logInfof("book review for %v|%v is already downloaded", parts[0], parts[1])
+		}
+	}
 
-    self.totalCount = self.bookList.Len()
-    logInfof("we got %v books to download this time", self.totalCount)
-    for elem := self.bookList.Front(); elem != nil; elem = elem.Next() {
-        review := elem.Value.(*BookReview)
-        logInfof("book review for %v|%v need to download", review.getBookId(), review.getBookTitle())
-    }
+	self.totalCount = self.bookList.Len()
+	logInfof("we got %v books to download this time", self.totalCount)
+	for elem := self.bookList.Front(); elem != nil; elem = elem.Next() {
+		review := elem.Value.(*BookReview)
+		logInfof("book review for %v|%v need to download", review.getBookId(), review.getBookTitle())
+	}
 
-    reviews := self.getReviewTask(3)
-    if len(reviews) == 0 {
-        logInfof("no task, exit!")
-        doneChan <- nil
-    }else {
-        for _, review := range reviews {
-            review.Start()
-        }
-    }
+	reviews := self.getReviewTask(3)
+	if len(reviews) == 0 {
+		logInfof("no task, exit!")
+		doneChan <- nil
+	} else {
+		for _, review := range reviews {
+			review.Start()
+		}
+	}
 
-    return nil
+	return nil
 }
 
 func (self *BookReviewStore) OnFinished(id string) {
-    self.doneCount +=1
-    self.doneMap.Store(id, TimeMillSecond())
+	self.doneCount += 1
+	self.doneMap.Store(id, TimeMillSecond())
 
-    logInfof("One Task is Done! Already downloaded %v books now", self.doneCount)
+	logInfof("One Task is Done! Already downloaded %v books now", self.doneCount)
 
-    if self.totalCount == self.doneCount {
-        //都完成了
-        logInfof("All Task is Done! total download %v resources", self.doneCount)
-        doneChan <- nil
+	if self.totalCount == self.doneCount {
+		//都完成了
+		logInfof("All Task is Done! total download %v resources", self.doneCount)
+		doneChan <- nil
 
-        //only for debug
-        logInfof("Check book list....")
-        for elem := self.bookList.Front(); elem != nil; elem = elem.Next() {
-            review := elem.Value.(*BookReview)
-            logInfof("book review for %v|%v still in book list", review.getBookId(), review.getBookTitle())
-        }
+		//only for debug
+		logInfof("Check book list....")
+		for elem := self.bookList.Front(); elem != nil; elem = elem.Next() {
+			review := elem.Value.(*BookReview)
+			logInfof("book review for %v|%v still in book list", review.getBookId(), review.getBookTitle())
+		}
 
-    }else {
-        reviews := self.getReviewTask(1)
-        for _, review := range reviews {
-            review.Start()
-        }
-    }
+	} else {
+		reviews := self.getReviewTask(1)
+		for _, review := range reviews {
+			review.Start()
+		}
+	}
 }
 
 func (self *BookReviewStore) getReviewTask(n int) (reviews []*BookReview) {
-    reviews = make([]*BookReview, 0)
-    if n < 1 {
-        return reviews
-    }
+	reviews = make([]*BookReview, 0)
+	if n < 1 {
+		return reviews
+	}
 
-    for{
-        elem := self.bookList.Front()
-        if elem == nil {
-            break
-        }
-        reviews = append(reviews, elem.Value.(*BookReview))
-        self.bookList.Remove(elem)
-        if len(reviews) == n {
-            return reviews
-        }
-    }
+	for {
+		elem := self.bookList.Front()
+		if elem == nil {
+			break
+		}
+		reviews = append(reviews, elem.Value.(*BookReview))
+		self.bookList.Remove(elem)
+		if len(reviews) == n {
+			return reviews
+		}
+	}
 
-    return reviews
+	return reviews
 }
-
